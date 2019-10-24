92827b22fa3fc718d2ec1da30bc46d986570c7fb
HuydDo <huydinhdo@yahoo.com>
2019-10-23 23:21:40 -0600

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,42 @@
+class ArtistsController < ApplicationController
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
+  end
+
+  private 
+
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
+
+end
+
+# // ♥  rake routes | grep artists                                                 
+#     artists GET    /artists(.:format)          artists#index
+#             POST   /artists(.:format)          artists#create
+#  new_artist GET    /artists/new(.:format)      artists#new
+# edit_artist GET    /artists/:id/edit(.:format) artists#edit
+#      artist GET    /artists/:id(.:format)      artists#show
+#             PATCH  /artists/:id(.:format)      artists#update
+#             PUT    /artists/:id(.:format)      artists#update
+#             DELETE /artists/:id(.:format)      artists#destroy
