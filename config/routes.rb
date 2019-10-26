6cfb5317520bfefb18df2f1a12e52172a2787466
maasa333 <konomaasa@gmail.com>
2019-10-26 16:18:26 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'genres/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :artists
   resources :genres
